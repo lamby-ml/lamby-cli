27e663e6513ecade7ad8b36ffd9c9c1ccac4dc86
Ammar Husain <ammo700@gmail.com>
2019-01-31 05:05:49 -0500

    Added init logic for basic config and log file

--- lamby.py
+++ lamby.py
@@ -1,6 +1,7 @@
 import click
 import os
 import shutil
+import json
 
 
 @click.group()
@@ -10,22 +11,33 @@ def cli():
 
 @cli.command()
 def init():
-    if os.path.isdir(os.getcwd() + '/.lamby'):
+    lamby_dir = os.getcwd() + '/.lamby'
+    if os.path.isdir(lamby_dir):
         click.echo('Lamby project already initialized in ' + os.getcwd())
         return
-    click.echo('Initializing Lamby project in ' + os.getcwd())
 
-    os.mkdir(os.getcwd() + '/.lamby')
+    os.mkdir(lamby_dir)
+
+    config_file = open(lamby_dir + '/config', "w+")
+    config_file.write(json.dumps({}))
+    config_file.close()
+
+    log_file = open(lamby_dir + '/log', "w+")
+    log_file.write(json.dumps([]))
+    log_file.close()
+
+    click.echo('Initializing Lamby project in ' + os.getcwd())
 
 
 @cli.command()
 def uninit():
-    if not os.path.isdir(os.getcwd() + '/.lamby'):
+    lamby_dir = os.getcwd() + '/.lamby'
+    if not os.path.isdir(lamby_dir):
         click.echo('Lamby project has not been initialized in ' + os.getcwd())
         return
     click.echo('Removing Lamby project in ' + os.getcwd())
 
-    shutil.rmtree(os.getcwd() + '/.lamby')
+    shutil.rmtree(lamby_dir)
 
 
 @cli.command()
