d85300eb71b8c6f35d1c45f0cde2a6f37a16985e
Ammar Husain <ammo700@gmail.com>
2019-01-31 15:24:50 -0500

    Added commit log logic with hashing

--- lamby/lamby.py
+++ lamby/lamby.py
@@ -2,6 +2,8 @@ import click
 import os
 import shutil
 import json
+import time
+import hashlib
 
 
 @click.group()
@@ -48,11 +50,65 @@ def commit(files, message):
     if not os.path.isdir(lamby_dir):
         click.echo('Lamby project has not been initialized in ' + os.getcwd())
         return
-    click.echo('Commiting following files:')
+
+    log = deserialize_log()
+
     for file in files:
-        click.echo('\t' + file)
-    if message is not None:
-        click.echo('Commit Message: ' + message)
+        if file not in log:
+            log[file] = []
+
+        commit_record = {}
+        commit_record["timestamp"] = int(time.time())
+        commit_record["message"] = message
+
+        str_to_hash = os.path.basename(file)
+        str_to_hash += str(commit_record["timestamp"])
+        str_to_hash += commit_record["message"]
+        str_to_hash += file_sha256(file)
+
+        if len(log[file]) > 0:
+            str_to_hash += log[file][-1]["hash"]
+
+        str_to_hash = str_to_hash.encode("utf-8")
+
+        commit_record["hash"] = hashlib.sha256(str_to_hash).hexdigest()
+        log[file].append(commit_record)
+
+    serialize_log(log)
+
+
+def deserialize_log():
+    if not os.path.isfile(os.getcwd() + '/.lamby/log'):
+        return {}
+    with open(os.getcwd() + '/.lamby/log') as log_file:
+        return json.load(log_file)
+
+
+def deserialize_config():
+    if not os.path.isfile(os.getcwd() + '/.lamby/config'):
+        return {}
+    with open(os.getcwd() + '/.lamby/config') as config_file:
+        return json.load(config_file)
+
+
+def serialize_log(data):
+    with open(os.getcwd() + '/.lamby/log', 'w+') as log_file:
+        log_file.write(json.dumps(data))
+        log_file.close()
+
+
+def serialize_config(data):
+    with open(os.getcwd() + '/.lamby/config', 'w+') as config_file:
+        config_file.write(json.dumps(data))
+        config_file.close()
+
+
+def file_sha256(fname):
+    hash_sha256 = hashlib.sha256()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
 
 
 if __name__ == '__main__':
