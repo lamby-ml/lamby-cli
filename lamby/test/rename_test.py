18956a8cac73f519c4ed09bf7544b14626eb912d
JennaEllis <ellis93@purdue.edu>
2019-02-18 23:24:46 -0500

    updated rename tests to include changes to meta

--- lamby/test/rename_test.py
+++ lamby/test/rename_test.py
@@ -1,6 +1,6 @@
 import os
 from src.rename import rename
-from src.utils import deserialize_log
+from src.utils import deserialize_log, deserialize_meta
 
 
 def test_rename_basic(runner):
@@ -8,20 +8,40 @@ def test_rename_basic(runner):
 
         os.mkdir('./.lamby')
         file = open('./.lamby/log', 'w+')
+        meta = open('./.lamby/meta', 'w+')
         log_str = '{\"test.onnx\": \"TESTDATA\"}'
+        meta_str = """{
+                        \"file_head\": {
+                        \"test.onnx\": \"TESTDATA\"
+                        },
+                        \"latest_commit\": {
+                        \"test.onnx\": \"TESTDATA\"
+                        }
+                        }
+                   """
 
         file.write(log_str)
+        meta.write(meta_str)
         file.close()
+        meta.close()
 
         result = runner.invoke(rename, ['test.onnx', 'renamed.onnx'])
 
         assert result.exit_code == 0
 
         log = deserialize_log()
+        meta = deserialize_meta()
 
         assert 'test.onnx' not in log
+        assert 'test.onnx' not in meta['file_head']
+        assert 'test.onnx' not in meta['latest_commit']
         assert 'renamed.onnx' in log
+        assert 'renamed.onnx' in meta['file_head']
+        assert 'renamed.onnx' in meta['latest_commit']
         assert log['renamed.onnx'] == 'TESTDATA'
+        assert log['renamed.onnx'] == 'TESTDATA'
+        assert meta['file_head']['renamed.onnx'] == 'TESTDATA'
+        assert meta['latest_commit']['renamed.onnx'] == 'TESTDATA'
 
 
 def test_rename_bad_file_name(runner):
