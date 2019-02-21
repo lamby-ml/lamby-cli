a1df75a7fb8a7759f8643a411237e31728530c4f
Daniel Kim <jicircle@naver.com>
2019-02-21 03:47:26 -0500

    Add tests to log and tag to check for invalid input (#24)

--- lamby/test/log_test.py
+++ lamby/test/log_test.py
@@ -29,3 +29,27 @@ File: test.onnx\n
 \tCommit ID: hash\n\tDate:      Thu Feb 14 17:08:42 2019 \n\
 \tMessage:   message\n\n"""
         assert result.output == correct_output
+
+
+def test_log_invalid_input(runner):
+    with runner.isolated_filesystem():
+
+        os.mkdir('./.lamby')
+        file = open('./.lamby/log', 'w+')
+        log_str = """
+        {
+            "test.onnx": [
+                {
+                    "timestamp": 1550182122,
+                    "message": "message",
+                    "hash": "hash"
+                }
+            ]
+        }
+        """
+        file.write(log_str)
+        file.close()
+
+        result = runner.invoke(log, ['wrong_file.onnx'])
+
+        assert result.exit_code == 1
--- lamby/test/tag_test.py
+++ lamby/test/tag_test.py
@@ -28,3 +28,26 @@ def test_tag_basic(runner):
         log = deserialize_log()
 
         assert log["test.onnx"][0]["tag"] == "TestTag"
+
+
+def test_tag_invalid_input(runner):
+    with runner.isolated_filesystem():
+        os.mkdir('./.lamby')
+        file = open('./.lamby/log', 'w+')
+        log_str = """
+        {
+            "test.onnx": [
+                {
+                    "timestamp": 1550098166,
+                    "message": "Test Commit",
+                    "hash": "testhash"
+                }
+            ]
+        }
+        """
+        file.write(log_str)
+        file.close()
+
+        result = runner.invoke(tag, ['incorrect_input'])
+
+        assert result.exit_code == 1
