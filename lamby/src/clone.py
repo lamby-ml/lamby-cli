cf0946b71e11e186b1d1c7dc5a2eecf9d726248a
Anoop Jain <anoopj08@gmail.com>
2019-03-27 20:50:56 -0400

    test

--- lamby/src/clone.py
+++ lamby/src/clone.py
@@ -3,14 +3,13 @@ import os
 
 
 @click.command('clone', short_help='clone a repository into current directory')
-@click.argument('repo_url', nargs=1)
-def clone(repo_url):
+@click.argument('project_id', nargs=1, short_help='ID of project to be cloned')
+def clone(project_id):
     """Clones a repository into the current directory via a URL"""
     from filestore import fs
 
     # need to get the repo name, using repo_url for now
     # the repo_url is the
 
-    dirname = "temp_dir"
-
+    dirname = project_id
 
