8df6d99b9a406c1b0dafa8e150aa09f1c71d84a4
Daniel Kim <jicircle@naver.com>
2019-03-28 18:58:53 -0400

    Complete lamby push functionality

--- lamby/filestore/__init__.py
+++ lamby/filestore/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+import gzip
 import boto3
 from botocore.client import Config
 from botocore.exceptions import ClientError
@@ -88,10 +88,12 @@ class Filestore(object):
     def upload_file(self, path, project_id):
         try:
             path = os.path.abspath(path)
-            with open(os.path.abspath(path), 'r') as f:
+            with gzip.open(os.path.abspath(path), 'r') as f:
                 base = os.path.basename(path)
                 key = os.path.splitext(base)[0]
-                self.default_bucket.put_object(Body=f, Key=key)
+                self.default_bucket.put_object(
+                    Body=f, Key=f'{project_id}/{key}',
+                    ContentType='text/plain')
         except ClientError as e:
             error_code = e.response['Error']['Code']
             if error_code == 'NoSuchBucket':
--- lamby/src/push.py
+++ lamby/src/push.py
@@ -4,7 +4,7 @@ import json
 import os
 import sys
 
-from src.utils import (deserialize_config, deserialize_log)
+from src.utils import (deserialize_config, deserialize_meta, deserialize_log)
 
 
 @click.command('push', short_help='push changes to lamby web')
@@ -25,8 +25,6 @@ def push():
     with open(gc_path) as global_config:
         gc_json = json.load(global_config)
 
-    click.echo(gc_json)
-
     log = deserialize_log()
     payload = {'log': log}
     header = {'x-auth': gc_json['api_key']}
@@ -40,7 +38,7 @@ def push():
     try:
         res = requests.post(os.getenv('LAMBY_WEB_URI') +
                             '/api/projects/status/{}'.format(project_id),
-                            json=payload, header=header)
+                            json=payload, headers=header)
     except requests.exceptions.ConnectionError:
         click.echo('Could not reach lamby web. Aborting push.')
         sys.exit(1)
@@ -57,8 +55,32 @@ def push():
 
     res_json = res.json()
     commits_to_upload = res_json['commits_to_upload']
+    from filestore import fs
     for filename in commits_to_upload:
         for commit in commits_to_upload[filename]:
-            click.echo(commit)
+            fs.upload_file(lamby_dir+'/commit_objects/' +
+                           commit['hash'], project_id)
 
+    payload = res_json
+    payload['meta'] = deserialize_meta()
+
+    try:
+        res = requests.post(os.getenv('LAMBY_WEB_URI') +
+                            '/api/projects/push/{}'.format(project_id),
+                            json=payload, headers=header)
+    except requests.exceptions.ConnectionError:
+        click.echo('Could not reach lamby web. Aborting push.')
+        sys.exit(1)
+    except requests.exceptions.Timeout:
+        click.echo('Connection timed out. Aborting push.')
+        sys.exit(1)
+    except requests.exceptions.TooManyRedirects:
+        click.echo(
+            'Too many redirects to reach lamby web. Aborting push.')
+        sys.exit(1)
+    except requests.exceptions.RequestException as e:
+        click.echo(e)
+        sys.exit(1)
+
+    click.echo(res.json()['message'])
     sys.exit(0)
